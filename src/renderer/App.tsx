daae09289f16bf67ddecc05768be003f1e97c678
Re4dMe <HellosAazS@gmail.com>
2024-10-22 14:05:45 +0800

    Move UI components to seperate file

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,187 +1,19 @@
 import { BrowserRouter, Router, Routes, Route, Link } from 'react-router-dom'
 import { TestComponent } from './TestPage'
 import { TestComponent2 } from './TestPage2'
-import {
-    FileTextIcon,
-    PlusIcon,
-    DotsHorizontalIcon,
-    PaperPlaneIcon,
-    Cross2Icon,
-    DrawingPinFilledIcon,
-    DragHandleHorizontalIcon,
-} from '@radix-ui/react-icons';
-  
+import { FileExplorer, NavMenu, MessageBoard } from './components/UIComponents'
 import './AppStyles.scss'
 import './styles.css';
-import { AppNavDropdownMenu } from './components/NavBar';
-import React, { useEffect, useState } from 'react'
-/*
-const textBubble = () => {
-
-    let onClickEvent = () => {
-        console.log('');
-    }
-
-    return (
-        <div className='speechbubble'>
-            <div className='header'>
-                <p> 
-                    So I created this thread to see if anyone wants to work on creating an N Wheeled class vehicle with me, as opposed to a bunch of people banging their heads against the wall seperately
-                </p>
-            </div>
-            <div className='footer'>
-                <div className='circular-menu'>
-                    <button className='round-button'>
-                        <DotsHorizontalIcon onClick={onClickEvent}/>
-                        <ul>
-                            <li className='menu-item'>
-                                <a>A</a>
-                            </li>
-                            <li className='menu-item'>
-                                <a>B</a>
-                            </li>
-                        </ul>
-                    </button>
-                </div>
-            </div>
-        </div>
-    );
-}
-*/
-
-interface textBubbleProps {
-    text: string;
-}
-
-const TextBubble = (props: textBubbleProps) => {
-
-    /*
-    useEffect(() => {
-        setTimeout(() => {
-            props.setLoadStatus('');
-        }, 5000)
-    }, []);
-    */
-
-    let onClickEvent = () => {
-        console.log('');
-    }
-
-    return (
-        <div className={`speechbubble`}>
-            <div className='header'>
-                <p> 
-                    { props.text }
-                </p>
-            </div>
-            <div className='footer'>
-                <div className='circular-menu'>
-                    <div className='icon'>
-                        <DotsHorizontalIcon />
-                    </div>
-                    <input type='checkbox' className='menu-toggler' />
-                    <ul>
-                        <li className='menu-item'>
-                            <div className='menu-item-background'></div>
-                            <a><DrawingPinFilledIcon /></a>
-                        </li>
-                        <li className='menu-item'>
-                            <div className='menu-item-background'></div>
-                            <a><DotsHorizontalIcon /></a>
-                        </li>
-                        <li className='menu-item'>
-                            <div className='menu-item-background'></div>
-                            <a><Cross2Icon /></a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    );
-}
+import React from 'react'
 
 const Apps = () => {
-    let [loadStatus, setLoadStatus] = useState('.load-in');
-    let [stateTextBubbleList, setTextBubbleList] : any = useState([])
-    const defaultText = 'So I created this thread to see if anyone wants to work on creating an N Wheeled class vehicle with me, as opposed to a bunch of people banging their heads against the wall seperately'
-
-    useEffect(() => {
-        setTextBubbleList([<TextBubble text={defaultText}/>, <TextBubble text={defaultText}/>]);
-    }, []); 
-
-    const addTextBubble = (): void => {
-        setTextBubbleList([
-            ...stateTextBubbleList,
-            <TextBubble text={defaultText}/>
-        ]);
-    }
-
     return (
         <>
             <div className='main-container'>
-                {
-                /*
-                <div className='block' 
-                    style={{
-                        backgroundColor: 'transparent', 
-                        width: '10%', 
-                        height: '5%', 
-                        gap: '15px',
-                    }}>
-                    <AppNavDropdownMenu />
-                    <button className='round-button'>
-                        <FileTextIcon />
-                    </button>
-                </div>
-                */
-                }
                 <div className='block'>
-                    <div className='nav-menu'>
-                        <input type='checkbox' id='menu-toggle' />
-                        <label className='button' htmlFor='menu-toggle'>
-                            <div className='menu-icon'>
-                                <span></span>
-                                <span></span>
-                                <span></span>
-                                <span></span>
-                            </div>
-                            <nav className='nav'>
-                                <ul>
-                                    <li><a><FileTextIcon /></a></li>
-                                    <li><a><FileTextIcon /></a></li>
-                                    <li><a><FileTextIcon /></a></li>
-                                </ul>
-                            </nav>
-                        </label>
-                    </div>
-                    <div className='input-group1'>
-                        <div className='button-group'>
-                            <button><FileTextIcon /></button>
-                            <button><PlusIcon /></button>
-                        </div>
-                        <label htmlFor="inp" className="inp">
-                            <input type='text' id="inp" placeholder="&nbsp;" />
-                            <span className="label">Label</span>
-                            <span className="focus-bg"></span>
-                        </label>
-                    </div>
-                    <div className='wrapper'>
-                        <div className='chat-window'>
-                        {
-                            stateTextBubbleList.map((name, i) => {
-                                return name;
-                            })
-                        }
-                        </div>
-                        <label htmlFor="user-text-input-id" className="label-user-input">
-                            <div className='user-input-box'>
-                            <textarea id='user-text-input-id' className='user-text-input' style={{ resize: 'none' }} placeholder="Type something..." />
-                            <button className='round-button'>
-                                    <PaperPlaneIcon onClick={addTextBubble}/>
-                            </button>
-                            </div>
-                        </label>
-                    </div>
+                    <NavMenu />
+                    <FileExplorer />
+                    <MessageBoard />
                 </div>
                 {
                     /*
--- src/renderer/TestPage.tsx
+++ src/renderer/TestPage.tsx
@@ -5,20 +5,8 @@ import React from 'react'
 export const TestComponent = () => {
     return (
         <div>
-            <Button className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white">
-                Save changes
-            </Button>
-
-            <button type="button" className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Alternative</button>
-            <Button className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
-                <Link to='/' style={{ textDecoration: 'none' }}>HomeA</Link>
-            </Button>
-            <Button className="rounded bg-sky-600 py-2 px-4 text-sm text-white data-[hover]:bg-sky-500 data-[active]:bg-sky-700">
-                <Link to='/test' style={{ textDecoration: 'none' }}>test</Link>
-            </Button>
-            <Button className="rounded bg-sky-600 py-2 px-4 text-sm text-white data-[hover]:bg-sky-500 data-[active]:bg-sky-700">
-                Test
-            </Button>
+            <Link to='/' style={{ textDecoration: 'none' }}>Home2</Link>
+            <Link to='/test2' style={{ textDecoration: 'none' }}>Test2</Link>
         </div>
     )
 }
